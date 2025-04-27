8ef7b02731332547334c4dc586bc74e4ff93cc3c
Gabo Guzman <gugd2001@hotmail.com>
2025-04-27 12:52:51 -0600

    add common models and configure cloudinary

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from os import getenv, path
 from loguru import logger
 from datetime import timedelta
+import cloudinary
 
 from dotenv import load_dotenv
 
@@ -178,6 +179,16 @@ CELERY_TASK_TIME_LIMIT = 5 * 60
 CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS = True
+
+CLOUDINARY_CLOUD_NAME = getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = getenv("CLOUDINARY_API_SECRET")
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+)
 
 LOGURU_LOGGING = {
     "handlers": [
